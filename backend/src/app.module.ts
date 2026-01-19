53515cc3983ce11e75f96c894f949de7c0334694
zihowl <fernimacias777@gmail.com>
2026-01-18 20:32:55 -0600

    chore: setup inicial del proyecto (monorepo)

---
+++ backend/src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    // 1. Cargar variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true, // Disponible en toda la app
+    }),
+
+    // 2. Configuración asíncrona de TypeORM (para leer las vars de entorno)
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // ¡OJO! Solo para desarrollo (crea tablas automáticamente)
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
